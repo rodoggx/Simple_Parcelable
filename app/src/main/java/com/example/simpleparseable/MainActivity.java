1cb8be90802caa0b7d0588871d92d3c53fef0b0f
rodoggx <mikerodolph@yahoo.com>
2016-08-03 16:35:42 -0400

    commit 2

--- app/src/main/java/com/example/simpleparseable/MainActivity.java
+++ app/src/main/java/com/example/simpleparseable/MainActivity.java
@@ -4,13 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String jsonString = "[{\"name\":\"Juan\",\"age\":20,\"grade\":8.1},{\"name\":\"Miguel\",\"age\":23,\"grade\":8.3},{\"name\":\"Roberto\",\"age\":39,\"grade\":9.3},{\"name\":\"Luis\",\"age\":19,\"grade\":6.9},{\"name\":\"Gaudencio\",\"age\":25,\"grade\":4.3}]";
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> dataArrayList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Type listType = new TypeToken<List<Student>>() {}.getType();
+        Gson gson = new GsonBuilder().create();
+
+        students = gson.fromJson(jsonComplexString, Result.class);
+
+        printList(students);
+
     }
 
     public void doMagic(View view) {
---
+++ app/src/main/java/com/example/simpleparseable/Student.java
@@ -0,0 +1,9 @@
+package com.example.simpleparseable;
+
+public class Student {
+
+    public String name;
+    public Integer age;
+    public Double grade;
+
+}
